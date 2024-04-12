b278cd5b61e8b095b35f804d96a5a8ad851ccb33
Queqz <111310573+queqz@users.noreply.github.com>
2024-04-12 20:14:46 +0200

    Uploaded the files

---
+++ builder.config.js
@@ -0,0 +1,22 @@
+options = {
+    username: 'Discord Bot',
+    avatar: 'https://cdn.discordapp.com/embed/avatars/1.png',
+    verified: false,
+    noUser: false,
+    data: null,
+    guiTabs: ['author', 'description'],
+    useJsonEditor: false,
+    reverseColumns: false,
+    allowPlaceholders: false,
+    autoUpdateURL: false,
+    autoParams: false,
+    hideEditor: false,
+    hidePreview: false,
+    hideMenu: false,
+    single: false,
+    noMultiEmbedsOption: false,
+    sourceOption: false,
+}
+
+addEventListener('DOMContentLoaded', () => {
+})
